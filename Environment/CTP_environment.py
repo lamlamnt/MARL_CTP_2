Change normalizing to base on the average oracle cost of the map instead of the realisation. Fix unit tests to reflect this change

Environment/CTP_environment.py
@@ -12,7 +12,10 @@ from tqdm import tqdm
 
 sys.path.append("..")
 from Utils import graph_functions
-from Utils.normalize_add_expensive_edge import add_expensive_edge
+from Utils.normalize_add_expensive_edge import (
+    add_expensive_edge,
+    get_expected_optimal_total_cost,
+)
 from Utils.optimal_combination import get_optimal_combination_and_cost
 import os
 
@@ -87,6 +90,14 @@ class MA_CTP_General(MultiAgentEnv):
                 )
 
                 # Normalize the weights using the expected optimal path length under full observability
+                normalizing_factor = get_expected_optimal_total_cost(
+                    graph_realisation, key
+                )
+                graph_realisation.graph.weights = jnp.where(
+                    graph_realisation.graph.weights != CTP_generator.NOT_CONNECTED,
+                    graph_realisation.graph.weights / normalizing_factor,
+                    CTP_generator.NOT_CONNECTED,
+                )
 
                 # Store the matrix of weights, blocking probs, and origin/goal
                 self.stored_graphs = self.stored_graphs.at[i, 0, :, :].set(
@@ -142,16 +153,6 @@ class MA_CTP_General(MultiAgentEnv):
             blocking_status,
             graph_origins,
             graph_goals,
-        )
-
-        # If don't normalize using full observability, can normalize here, after adding expensive edge
-        _, normalizing_factor = get_optimal_combination_and_cost(
-            graph_weights, blocking_status, graph_origins, graph_goals, self.num_agents
-        )
-        graph_weights = jnp.where(
-            graph_weights != CTP_generator.NOT_CONNECTED,
-            graph_weights / normalizing_factor,
-            CTP_generator.NOT_CONNECTED,
         )
 
         env_state = self.__convert_graph_realisation_to_state(

Unit_Tests/test_CTP_environment.py
@@ -72,7 +72,7 @@ def test_step(printer, environment: CTP_environment.MA_CTP_General):
     env_state_4, belief_state_4, rewards_4, done_4, subkey = environment.step(
         subkey, initial_env_state, initial_belief_states, jnp.array([2, 3])
     )
-    assert jnp.isclose(rewards_4, jnp.array([-0.1464, -0.854]), rtol=1e-3).all()
+    assert jnp.isclose(rewards_4, jnp.array([-0.1407, -0.821]), rtol=1e-3).all()
 
     # make sure positions are correct
     assert jnp.sum(env_state_4[0, :2, :]) == 2
@@ -86,7 +86,7 @@ def test_step(printer, environment: CTP_environment.MA_CTP_General):
         subkey, env_state_4, belief_state_4, jnp.array([5, 2])
     )
     assert jnp.array_equal(done_5, jnp.array([True, False]))
-    assert jnp.isclose(rewards_5, jnp.array([-0.1, -0.89]), rtol=1e-2).all()
+    assert jnp.isclose(rewards_5, jnp.array([-0.1, -0.856]), rtol=1e-2).all()
     assert not jnp.array_equal(env_state_5[3, :, :], env_state_4[3, :, :])
     assert jnp.array_equal(env_state_5[3, :, :], belief_state_5[0, 3, :, :])
     assert jnp.array_equal(env_state_5[3, :, :], belief_state_5[1, 3, :, :])
@@ -106,7 +106,7 @@ def test_step(printer, environment: CTP_environment.MA_CTP_General):
     env_state_6, belief_state_6, rewards_6, done_6, subkey = environment.step(
         subkey, env_state_5, belief_state_5, jnp.array([0, 3])
     )
-    assert jnp.isclose(rewards_6, jnp.array([0, -0.89]), rtol=1e-2).all()
+    assert jnp.isclose(rewards_6, jnp.array([0, -0.856]), rtol=1e-2).all()
     assert jnp.array_equal(done_6, jnp.array([True, False]))
 
     # check symmetrical
@@ -144,7 +144,7 @@ def test_same_goal(printer, environment: CTP_environment.MA_CTP_General):
     env_state_1, belief_state_1, rewards_1, done_1, subkey = environment.step(
         subkey, initial_env_state, initial_belief_states, jnp.array([0, 0])
     )
-    assert jnp.isclose(rewards_1, jnp.array([-200, -0.207]), rtol=1e-3).all()
+    assert jnp.isclose(rewards_1, jnp.array([-200, -0.1991]), rtol=1e-3).all()
     env_state_2, belief_state_2, rewards_2, done_2, subkey = environment.step(
         subkey, env_state_1, belief_state_1, jnp.array([2, 2])
     )
@@ -196,7 +196,7 @@ def test_single_agent_working(printer):
     assert terminate_4 == jnp.bool_(True)
     assert jnp.isclose(reward_4[0], -0.1, rtol=1e-3)
     assert jnp.isclose(
-        reward_1[0] + reward_2[0] + reward_3[0] + reward_4[0], -1.1, rtol=1e-2
+        reward_1[0] + reward_2[0] + reward_3[0] + reward_4[0], -1.6422, rtol=1e-2
     )
 
 

Unit_Tests/test_optimal_combination_baseline.py
@@ -37,7 +37,7 @@ def test_get_optimal_combination_and_cost(
         2,
     )
     assert best_combination.shape == (2,)
-    assert jnp.isclose(best_combination_cost, 1, rtol=1e-2)
+    assert jnp.isclose(best_combination_cost, 0.9617, rtol=1e-2)
     assert jnp.array_equal(best_combination, jnp.array([0, 1]))
 
 
@@ -141,7 +141,7 @@ def test_optimistic_agent_full_episode(
     total_cost = optimistic_agent.get_total_cost(
         environment, initial_belief_states, initial_env_state, key
     )
-    assert jnp.isclose(total_cost, 1.2, rtol=1e-2)
+    assert jnp.isclose(total_cost, 1.161, rtol=1e-2)
 
 
 # test get_optimal_combination_and_cost for 3 agents
@@ -160,7 +160,7 @@ def test_get_optimal_combination_and_cost_3_agents(printer):
         3,
     )
     assert best_combination.shape == (3,)
-    assert jnp.isclose(best_combination_cost, 1, rtol=1e-2)
+    assert jnp.isclose(best_combination_cost, 0.899, rtol=1e-2)
     assert jnp.array_equal(best_combination, jnp.array([0, 1, 2]))
 
 

Utils/normalize_add_expensive_edge.py
@@ -5,11 +5,10 @@ import sys
 
 sys.path.append("..")
 from Environment import CTP_generator
+from Utils import graph_functions
+from Utils.optimal_combination import get_optimal_combination_and_cost
 
-
-# Normalize by the total cost/num_agents (if is unstable, can try normalize by total cost)
-def normalize(weights: jnp.ndarray, num_agents: int) -> jnp.ndarray:
-    pass
+NUM_SAMPLES_FACTOR = 10
 
 
 # Check solvability and add expensive edge in one go
@@ -156,3 +155,33 @@ def get_solvability_matrix(
         _origin_goal_pair_is_solvable, in_axes=(None, None, None, None, 0)
     )(weights, blocking_status, origins, goals, jnp.arange(num_agents))
     return solvability_matrix
+
+
+def get_expected_optimal_total_cost(
+    graphRealisation: CTP_generator.CTPGraph_Realisation, key: jax.random.PRNGKey
+) -> float:
+    # Sample blocking status for several times and get the average path length
+    num_agents = graphRealisation.graph.num_agents
+    num_samples = NUM_SAMPLES_FACTOR * graphRealisation.graph.n_nodes
+    many_keys = jax.random.split(key, num=num_samples)
+
+    def get_sampled_optimal_cost_for_one_instance(key):
+        blocking_status = graphRealisation.sample_blocking_status(key)
+        graph_weights, graph_blocking_prob, blocking_status = add_expensive_edge(
+            graphRealisation.graph.weights,
+            graphRealisation.graph.blocking_prob,
+            blocking_status,
+            graphRealisation.graph.origin,
+            graphRealisation.graph.goal,
+        )
+        _, one_normalizing_factor = get_optimal_combination_and_cost(
+            graph_weights,
+            blocking_status,
+            graphRealisation.graph.origin,
+            graphRealisation.graph.goal,
+            num_agents,
+        )
+        return one_normalizing_factor
+
+    normalizing_factor = jax.vmap(get_sampled_optimal_cost_for_one_instance)(many_keys)
+    return jnp.mean(normalizing_factor)