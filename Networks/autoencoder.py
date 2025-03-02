a7e4113540947c5cf761f6d44421d1626fd1b774
lamlamnt <lam.lam@pmb.ox.ac.uk>
2025-03-02 16:00:53 +0000

    Fix bug with onelayernet

--- Networks/autoencoder.py
+++ Networks/autoencoder.py
@@ -159,3 +159,9 @@ class Autoencoder(nn.Module):
             latent_representation
         )  # Decode latent representation back to data space
         return latent_representation, reconstructed
+
+
+class OneLayerNet(nn.Module):
+    @nn.compact
+    def __call__(self, x):
+        return x
